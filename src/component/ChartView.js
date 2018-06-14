4dcd26dd8e9b85b5ca77ce3c7b0bdc2733e45d91
도 중은 <yepp4you@neowiz.com>
2018-06-14 10:10:32 +0900

    css변경

--- src/component/ChartView.js
+++ src/component/ChartView.js
@@ -6,7 +6,7 @@ import icon from '../static/images/dashboard__icon--transactions.png';
 const Chart = () => (
   <div className={styles.graph}>
     <img src={icon} alt=""/>
-    <h3>Transactions</h3>
+    <h3>Transactions <font style={{fontSize : 'small'}}> trx / round </font></h3>
     <div className={styles.graph__content}>
       <TransactionChart/>
     </div>
